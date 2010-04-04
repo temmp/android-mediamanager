c47cb4ae618452fa9d785cfee1b8c693f7839e1c
langnerjoerg <langnerjoerg@edaa106d-4bb0-6de8-4e58-53c418c94a9e>
2010-04-04 23:36:58 +0000

    should have been in last commit..just forgot it

---
+++ branches/MediaManager/src/local/mediamanager/view/GiveMediaBack.java
@@ -0,0 +1,13 @@
+package local.mediamanager.view;
+
+import android.os.Bundle;
+import local.mediamanager.view.sharedmenues.SharedActivity;
+
+public class GiveMediaBack extends SharedActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
