5a9e73701e5f7b39d8dd8e067b854befbde21947
langnerjoerg <langnerjoerg@edaa106d-4bb0-6de8-4e58-53c418c94a9e>
2010-04-11 14:49:21 +0000

    implemented calendar

--- src/local/mediamanager/listener/LendMediaScanListener.java
+++ src/local/mediamanager/listener/LendMediaScanListener.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import local.mediamanager.R;
 import local.mediamanager.model.Media;
+import local.mediamanager.util.Calendar;
 import local.mediamanager.util.Contact;
 import local.mediamanager.util.Date;
 import local.mediamanager.util.xml.XMLMediaFileEditor;
@@ -11,14 +12,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
- * Listener wenn ein eingescanntes Medium verliehen wird. Ermittelt die in der GUI
- * eingegebenen Medieneigenschaften und veranlasst deren Speicherung in das XML
- * Dokument.
+ * Listener wenn ein eingescanntes Medium verliehen wird. Ermittelt die in der
+ * GUI eingegebenen Medieneigenschaften und veranlasst deren Speicherung in das
+ * XML Dokument.
  * 
  * @author Joerg Langner
  */
@@ -26,39 +28,50 @@ public class LendMediaScanListener implements OnClickListener {
 
 	// Informationstext wenn Medium angelegt wurde
 	private final String MEDIA_SUCCESSFULLY_LENT = "Medium erfolgreich verliehen.";
-	
+
 	private Activity activity;
 	private final String barcode;
-	
-	public LendMediaScanListener(Activity activity, String barcode){
+
+	public LendMediaScanListener(Activity activity, String barcode) {
 		this.activity = activity;
 		this.barcode = barcode;
 	}
-	
+
 	@Override
 	public void onClick(View arg0) {
 		Spinner spLendto = (Spinner) activity.findViewById(R.id.spLendto);
-		DatePicker dpLendtime = (DatePicker) activity.findViewById(R.id.dpLendtime);
+		DatePicker dpLendtime = (DatePicker) activity
+				.findViewById(R.id.dpLendtime);
+		// die ID des Kontaktes wird anhand des im Spinner ausgewaehlten
+		// Kontaktes ermittelt
 		Contact contacts = new Contact(activity);
-		HashMap<Integer, Integer> contactIDMap = contacts
-				.getContactIDMap();
-		Date dateObject = new Date(dpLendtime.getDayOfMonth(),
-				dpLendtime.getMonth() + 1, dpLendtime.getYear());
+		HashMap<Integer, Integer> contactIDMap = contacts.getContactIDMap();
+		// Ausleihdatum ermitteln
+		Date dateObject = new Date(dpLendtime.getDayOfMonth(), dpLendtime
+				.getMonth() + 1, dpLendtime.getYear());
 		String date = dateObject.getDate();
-		XMLMediaFileEditor xmlEditor = new XMLMediaFileEditor(
-				activity);
+		XMLMediaFileEditor xmlEditor = new XMLMediaFileEditor(activity);
 		Media media = xmlEditor.getMediaByBarcode(barcode);
-		media.setOwner(contactIDMap.get(
-				spLendto.getSelectedItemPosition()).toString());
+		media.setOwner(contactIDMap.get(spLendto.getSelectedItemPosition())
+				.toString());
 		media.setDate(date);
 		media.setStatus(Media.STATUS.VERLIEHEN.getName());
 		// Medium updaten
 		xmlEditor.updateMediaByBarcode(barcode, media);
-		Context context = activity.getApplicationContext();
+		// Kalendereintrag erstellen falls die Checkbox gecheckt ist
+		CheckBox calenderEntry = (CheckBox) activity
+				.findViewById(R.id.cbCalEntry);
+		if (calenderEntry.isChecked()) {
+			Calendar calendar = new Calendar(activity);
+			calendar.addCalendarEntry(dpLendtime.getYear(), dpLendtime
+					.getMonth() + 1, dpLendtime.getDayOfMonth(), media
+					.getTitle());
+		}
 		// nachricht an Benutzer das medium erfolgreich
 		// verliehen wurde
-		Toast.makeText(context, MEDIA_SUCCESSFULLY_LENT,
-				Toast.LENGTH_SHORT).show();
+		Context context = activity.getApplicationContext();
+		Toast.makeText(context, MEDIA_SUCCESSFULLY_LENT, Toast.LENGTH_SHORT)
+				.show();
 		activity.finish();
 	}
 
