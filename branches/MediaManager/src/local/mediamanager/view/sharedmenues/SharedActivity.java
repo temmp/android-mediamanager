20d4b41850b98c3a8be570905d87a3f513f7c90c
langnerjoerg <langnerjoerg@edaa106d-4bb0-6de8-4e58-53c418c94a9e>
2010-04-13 18:22:29 +0000

    licence

--- branches/MediaManager/src/local/mediamanager/view/sharedmenues/SharedActivity.java
+++ branches/MediaManager/src/local/mediamanager/view/sharedmenues/SharedActivity.java
@@ -31,7 +31,7 @@ public class SharedActivity extends Activity {
 
 	// Beschriftungen Info Dialog
 	private static final String INFO_DIALOG_TITLE = "Über MediaManager";
-	private static final String INFO_DIALOG_CONTENT = "Copyright 2010 Jörg Langner & Andreas Wiedemann. \nLicensed under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.";
+	private static final String INFO_DIALOG_CONTENT = "Copyright 2010 Jörg Langner & Andreas Wiedemann \nLicensed under the Apache License, Version 2.0";
 	private static final String INFO_DIALOG_URL = "<a href=\"http://code.google.com/p/android-mediamanager\";>MediaManager Projekt Homepage</a><br>";
 	private static final String BUTTON_LABEL = "OK";
 
@@ -56,11 +56,12 @@ public class SharedActivity extends Activity {
 			// Textinhalt des Dialogs
 			TextView content = new TextView(this);
 			content.setText(INFO_DIALOG_CONTENT);
-			content.setGravity(Gravity.LEFT);
+			content.setGravity(Gravity.CENTER);
 			// klickbarer Link im Dialog
 			TextView link = new TextView(this);
 			link.setText(Html.fromHtml(INFO_DIALOG_URL));
 			link.setMovementMethod(LinkMovementMethod.getInstance());
+			link.setGravity(Gravity.CENTER);
 			// OK Button
 			Button btOK = new Button(this);
 			btOK.setText(BUTTON_LABEL);
